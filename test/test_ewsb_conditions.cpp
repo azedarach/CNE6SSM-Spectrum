29d91c7a15278a9b03c88a3a4d5dbaf773b87dc5
Dylan Harries <dylan.harries@adelaide.edu.au>
2014-09-15 23:12:07 +0930

    Added skeleton of test module

---
+++ test/test_ewsb_conditions.cpp
@@ -0,0 +1,28 @@
+// ====================================================================
+// Test suite for implementation of EWSB conditions
+// ====================================================================
+
+#include "CNE6SSM_two_scale_model.hpp"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE test_ewsb_conditions
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+using namespace std;
+using namespace flexiblesusy;
+
+// Test that the default EWSB conditions give
+// expected values for a set of parameters
+BOOST_AUTO_TEST_CASE( test_default_ewsb )
+{
+
+}
+
+// Test that the custom EWSB conditions give
+// expected values for a set of parameters
+BOOST_AUTO_TEST_CASE( test_custom_ewsb )
+{
+
+}
