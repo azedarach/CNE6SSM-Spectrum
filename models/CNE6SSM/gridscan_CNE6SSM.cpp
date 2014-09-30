ff00d94d6ecff275bc3e3d5fcf806769f94b11ae
Dylan Harries <dylan.harries@adelaide.edu.au>
2014-09-30 17:31:40 +0930

    Added temporary initialisation routine in grid scan_CNE6SSM.cpp

--- models/CNE6SSM/gridscan_CNE6SSM.cpp
+++ models/CNE6SSM/gridscan_CNE6SSM.cpp
@@ -1,5 +1,8 @@
 // ====================================================================
 // Does a grid scan of the CNE6SSM parameter space
+//   - trial version before generalising it to arbitrary input
+//     parameters (by which I mean arbitrary scans over the 
+//     parameters defined in CNE6SSM_input_parameters)
 // ====================================================================
 
 #include "CNE6SSM_input_parameters.hpp"
@@ -12,7 +15,7 @@
 
 #include <iostream>
 
-void get_current_inputs(const std::vector<std::size_t>&, flexiblesusy::CNE6SSM_input_parameters&);
+void get_current_inputs(const std::vector<std::size_t>&, const std::vector<std::size_t>&, flexiblesusy::CNE6SSM_input_parameters&);
 
 int main()
 {
@@ -25,10 +28,10 @@ int main()
    oneset.toMz();
 
    CNE6SSM_spectrum_generator<algorithm_type> spectrum_generator;
-   spectrum_generator.set_precision_goal(1.0e-4);
+   spectrum_generator.set_precision_goal(5.0e-3);
    spectrum_generator.set_max_iterations(0);   // 0 == automatic
    spectrum_generator.set_calculate_sm_masses(0); // 0 == no
-   spectrum_generator.set_alternate_ewsb(1); // 1 == yes
+   spectrum_generator.set_alternate_ewsb(0); // 1 == yes
    spectrum_generator.set_parameter_output_scale(0); // 0 == susy scale
 
    std::size_t m0_npts = 5;
@@ -49,7 +52,7 @@ int main()
         << '\n';
 
    while (!scan.has_finished()) {
-      get_current_inputs(scan.get_position(), input);
+      get_current_inputs(scan.get_position(), scan.get_dimensions(), input);
 
       spectrum_generator.run(oneset, input);
 
@@ -77,7 +80,90 @@ int main()
    return 0;
 }
 
-void get_current_inputs(const std::vector<std::size_t>& posn, flexiblesusy::CNE6SSM_input_parameters&)
+void get_current_inputs(const std::vector<std::size_t>& posn, const std::vector<std::size_t>& dims, flexiblesusy::CNE6SSM_input_parameters& input)
 {
-   
+   const double m0_lower = 0.; // GeV
+   const double m0_upper = 3000.; // GeV
+   const double m12_lower = 0.; // GeV
+   const double m12_upper = 3000.; // GeV
+   const double TanBeta_lower = 10.;
+   const double TanBeta_upper = 10.;
+   const double Azero_lower = 1000.; // GeV
+   const double Azero_upper = 1000.; // GeV
+
+   double m0_incr = 0.0;
+   double m12_incr = 0.0;
+   double Azero_incr = 0.0;
+
+   if (dims.at(0) > 1) 
+      m0_incr = (m0_upper - m0_lower) / (dims.at(0) - 1.0);
+   if (dims.at(1) > 1) 
+      m12_incr = (m12_upper - m12_lower) / (dims.at(1) - 1.0);
+   if (dims.at(2) > 1) 
+      Azero_incr = (Azero_upper - Azero_lower) / (dims.at(2) - 1.0);
+
+   input.m0 = m0_lower + m0_incr * posn.at(0);
+   input.m12 = m12_lower + m12_incr * posn.at(1);
+   input.TanBeta = TanBeta_lower;
+   input.Azero = Azero_lower + Azero_incr * posn.at(2);
+
+   input.SignLambdax = 1;
+   input.ssumInput = 40000.0; // GeV
+   input.QS = 5.;
+
+   input.hEInput(0,0) = 0.;
+   input.hEInput(0,1) = 0.;
+   input.hEInput(1,0) = 0.;
+   input.hEInput(1,1) = 0.;
+   input.hEInput(2,0) = 0.;
+   input.hEInput(2,1) = 0.;
+
+   input.SigmaLInput = 3.0e-1;
+   input.KappaPrInput = 2.0e-2;
+   input.SigmaxInput = 1.0e-1;
+
+   input.gDInput(0,0) = 0.;
+   input.gDInput(0,1) = 0.;
+   input.gDInput(0,2) = 0.;
+   input.gDInput(1,0) = 0.;
+   input.gDInput(1,1) = 0.;
+   input.gDInput(1,2) = 0.;
+   input.gDInput(2,0) = 0.;
+   input.gDInput(2,1) = 0.;
+   input.gDInput(2,2) = 0.;
+
+   input.KappaInput(0,0) = 2.0e-1;
+   input.KappaInput(0,1) = 0.;
+   input.KappaInput(0,2) = 0.;
+   input.KappaInput(1,0) = 0.;
+   input.KappaInput(1,1) = 2.0e-1;
+   input.KappaInput(1,2) = 0.;
+   input.KappaInput(2,0) = 0.;
+   input.KappaInput(2,1) = 0.;
+   input.KappaInput(2,2) = 2.0e-1;
+
+   input.Lambda12Input(0,0) = 5.0e-1;
+   input.Lambda12Input(0,1) = 0.;
+   input.Lambda12Input(1,0) = 0.;
+   input.Lambda12Input(1,1) = 5.0e-1;
+
+   input.fuInput(0,0) = 1.0e-7;
+   input.fuInput(0,1) = 0.;
+   input.fuInput(1,0) = 0.;
+   input.fuInput(1,1) = 1.0e-7;
+   input.fuInput(2,0) = 1.0e-7;
+   input.fuInput(2,1) = 0.;
+
+   input.fdInput(0,0) = 1.0e-7;
+   input.fdInput(0,1) = 0.;
+   input.fdInput(1,0) = 0.;
+   input.fdInput(1,1) = 1.0e-7;
+   input.fdInput(2,0) = 0.;
+   input.fdInput(2,1) = 1.0e-7;
+
+   input.MuPrInput = 5.0e2;
+   input.MuPhiInput = 0.;
+   input.BMuPrInput = 5.0e2;
+   input.BMuPhiInput = 0.;
+
 }
