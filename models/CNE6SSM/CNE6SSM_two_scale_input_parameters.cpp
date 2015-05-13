80ec22964b6d55cd3697e5e71bf463616d7a8e38
Dylan Harries <dylan.harries@adelaide.edu.au>
2015-05-13 11:58:31 +0930

    Revert "Added function for getting SLHA info about input parameters"
    This reverts commit c0d40aaf6130125895927d1397f708233009c8ab.
    
    Introducing functions into the input parameters structs would in
    general require error checking. This could be done later (with some
    thought) but for now it will be easier to overload the read/write
    routines for the two specialisations of input parameters.

--- models/CNE6SSM/CNE6SSM_two_scale_input_parameters.cpp
+++ models/CNE6SSM/CNE6SSM_two_scale_input_parameters.cpp
@@ -35,34 +35,4 @@ std::ostream& operator<<(std::ostream& ostr, const CNE6SSM_input_parameters<Two_
    return ostr;
 }
 
-CNE6SSM_input_parameters_slha CNE6SSM_input_parameters<Two_scale>::get_slha_format() const
-{
-   CNE6SSM_input_parameters_slha slha_format;
-
-   slha_format.minpar.push_back(SLHA_input_entry("MINPAR", "m0", 1));
-   slha_format.minpar.push_back(SLHA_input_entry("MINPAR", "m12", 2));
-   slha_format.minpar.push_back(SLHA_input_entry("MINPAR", "TanBeta", 3));
-   slha_format.minpar.push_back(SLHA_input_entry("MINPAR", "SignLambdax", 4));
-   slha_format.minpar.push_back(SLHA_input_entry("MINPAR", "Azero", 5));
-
-   slha_format.extpar.push_back(SLHA_input_entry("EXTPAR", "ssumInput", 65));
-   slha_format.extpar.push_back(SLHA_input_entry("EXTPAR", "QSInput", 72));
-
-   slha_format.entries.push_back(SLHA_input_entry("HMIXIN", "MuPhiInput", 31));
-   slha_format.entries.push_back(SLHA_input_entry("HMIXIN", "KappaPrInput", 32));
-   slha_format.entries.push_back(SLHA_input_entry("HMIXING", "SigmaxInput", 33));
-   slha_format.blocks.push_back(SLHA_input_block("ESIXHEYUKIN", "hEInput"));
-   slha_format.entries.push_back(SLHA_input_entry("ESIXRUNIN", "SigmaLInput", 42));
-   slha_format.blocks.push_back(SLHA_input_block("ESIXGDYUKIN", "gDInput"));
-   slha_format.blocks.push_back(SLHA_input_block("ESIXFUYUKIN", "fuInput"));
-   slha_format.blocks.push_back(SLHA_input_block("ESIXFDYUKIN", "fdInput"));
-   slha_format.entries.push_back(SLHA_input_entry("ESIXRUNIN", "BMuPhiInput", 30));
-   slha_format.blocks.push_back(SLHA_input_block("ESIXKAPPAIN", "KappaInput"));
-   slha_format.blocks.push_back(SLHA_input_block("ESIXLAMBDAIN", "Lambda12Input"));
-   slha_format.entries.push_back(SLHA_input_entry("ESIXRUNIN", "MuPrInput", 0));
-   slha_format.entries.push_back(SLHA_input_entry("ESIXRUNIN", "BMuPrInput", 101));
-
-   return slha_format;
-}
-
 } // namespace flexiblesusy
--- models/CNE6SSM/CNE6SSM_two_scale_input_parameters.hpp
+++ models/CNE6SSM/CNE6SSM_two_scale_input_parameters.hpp
@@ -7,7 +7,6 @@
 #define CNE6SSM_TWO_SCALE_INPUT_PARAMETERS_H
 
 #include "CNE6SSM_input_parameters.hpp"
-#include "CNE6SSM_input_parameters_slha.hpp"
 
 #include <complex>
 #include <Eigen/Core>
@@ -49,9 +48,6 @@ struct CNE6SSM_input_parameters<Two_scale> {
    BMuPhiInput(0)
 
    {}
-
-   CNE6SSM_input_parameters_slha get_slha_format() const;
-
 };
 
 std::ostream& operator<<(std::ostream&, const CNE6SSM_input_parameters<Two_scale>&);
